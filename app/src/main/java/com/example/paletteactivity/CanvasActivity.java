38c3ff39eb16e1010405ed410ec310ce55c1cd2b
Theodore <tuf94849@temple.edu>
2019-02-14 12:37:47 -0500

    Send background color from first activity to second activity. Changed background color with the background color selected in the first activity. Part 1 finished.

--- app/src/main/java/com/example/paletteactivity/CanvasActivity.java
+++ app/src/main/java/com/example/paletteactivity/CanvasActivity.java
@@ -1,5 +1,6 @@
 package com.example.paletteactivity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class CanvasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+
+        String selectedColor = (getIntent().getStringExtra("colorPicked"));
+        getWindow().getDecorView().setBackgroundColor(Color.parseColor(selectedColor));
     }
 }
